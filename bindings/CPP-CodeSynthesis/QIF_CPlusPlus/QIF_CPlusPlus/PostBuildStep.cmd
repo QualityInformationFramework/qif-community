c7c16b87e24bf29e12e0b0505e403c146944eda0
Daniel Campbell <campbell.daniel.a@gmail.com>
2025-05-02 12:07:21 -0500

    Updated projects to use relative paths

--- bindings/CPP-CodeSynthesis/QIF_CPlusPlus/QIF_CPlusPlus/PostBuildStep.cmd
+++ bindings/CPP-CodeSynthesis/QIF_CPlusPlus/QIF_CPlusPlus/PostBuildStep.cmd
@@ -12,12 +12,12 @@ exit /b 1
 
 :release
 @echo on
-copy /Y /D "C:\dev\qif-community\bindings\CPP-CodeSynthesis\QIF_30\x64\Release\QIF_30.dll" "%~1"
-copy /Y /D "C:\dev\xsd\bin\release\xerces-c-3.2.dll" "%~1"
+copy /Y /D "..\..\..\..\..\qif-community\bindings\CPP-CodeSynthesis\QIF_30\x64\Release\QIF_30.dll" "%~1"
+copy /Y /D "..\..\..\..\..\xsd\bin\release\xerces-c-3.2.dll" "%~1"
 exit /b 0
 
 :debug
 @echo on
-copy /Y /D "C:\dev\qif-community\bindings\CPP-CodeSynthesis\QIF_30\x64\Debug\QIF_30D.dll" "%~1"
-copy /Y /D "C:\dev\qif-community\bindings\CPP-CodeSynthesis\QIF_30\x64\Debug\QIF_30D.pdb" "%~1"
-copy /Y /D "C:\dev\xsd\bin\debug\xerces-c-3.2.dll" "%~1"
+copy /Y /D "..\..\..\..\..\qif-community\bindings\CPP-CodeSynthesis\QIF_30\x64\Debug\QIF_30D.dll" "%~1"
+copy /Y /D "..\..\..\..\..\qif-community\bindings\CPP-CodeSynthesis\QIF_30\x64\Debug\QIF_30D.pdb" "%~1"
+copy /Y /D "..\..\..\..\..\xsd\bin\debug\xerces-c-3.2.dll" "%~1"
